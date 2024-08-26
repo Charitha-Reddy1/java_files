03e4855fa4fc28fa69b552cbc6f5a81dc8cfc1ec
Charitha-Reddy1 <carg2800@gmail.com>
2024-08-26 20:46:33 +0530

    Add files via upload

---
+++ f.java
@@ -0,0 +1,8 @@
+class data
+{
+    public static void main(String [] args)
+    {
+        String a="Charitha";int b=99;byte c=5;short d=72;double e=9.994;float f=6.321f;long g=123456;
+        System.out.print("char "+a+"\nint "+b+"\nbyte "+c+"\nshort "+d+"\ndouble "+e+"\nfloat "+f+"\nlong "+g);
+    }
+}
