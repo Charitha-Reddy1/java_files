8d9f0cfc2eb3ae22536b733770413e72ee52c3b0
Charitha-Reddy1 <carg2800@gmail.com>
2024-09-01 22:45:16 +0530

    Add files via upload

--- con.java
+++ con.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 
 class Scratch {
     public static void main(String[] args) {
-        Scanner tym=new Scanner(System.in);
-        int time=tym.nextInt();
-        if(time>1&&time<12){
+        Scanner tym = new Scanner(System.in);
+        System.out.println("Enter a timing in a day:");
+        int time = tym.nextInt();
+        if (time > 1 && time < 12) {
             System.out.println("Did you have your breakfast");
-        }
-        if(time>12&&time<18){
+        } else if (time > 12 && time < 18) {
             System.out.println("Did you have your lunch");
-        }
-        if(time>18&&time<24){
+        } else if (time > 18 && time < 24) {
             System.out.println("Did you have your dinner");
+        } else {
+            System.out.println("Invalid timing");
         }
+
+        char shie = 't';
+        int re = (shie >'s') ? /*true*/ 0 :/*false*/ 1;
+        System.out.println(re);
     }
 }
+
