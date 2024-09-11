b5c01f024de95c757db19eb45f4dbab665476361
Charitha-Reddy1 <carg2800@gmail.com>
2024-09-11 22:39:51 +0530

    Add files via upload

---
+++ meth2.java
@@ -0,0 +1,12 @@
+class Scratch {
+    static String str(String a){
+        return a;
+    }
+    static float str(float b){
+        return b;
+    }
+    public static void main(String[] args) {
+        System.out.println("str name : "+str("Hello"));
+        System.out.println("float num : "+str(9.876f));
+    }
+}
